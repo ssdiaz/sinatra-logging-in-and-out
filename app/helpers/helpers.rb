96bc6e6e26aea7feb0caaa8babd862639df30fb9
Samantha Diaz <samidiaz3@gmail.com>
2021-11-18 20:19:42 -0500

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,11 @@
 class Helpers
+    def self.current_user(session)
+        #finds the user in the database and returns the user
+        @user = User.find_by_id(session[:user_id])
+    end
+
+    def self.is_logged_in?(session)
+        #returns true if userid is in the session hash
+        !!session[:user_id]
+    end
 end
